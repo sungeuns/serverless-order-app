d64606060905f64da389dbae876bf8a5e1816459
EC2 Default User <ec2-user@ip-172-31-40-67.ap-northeast-2.compute.internal>
2023-03-31 14:38:50 +0000

    added lab2 contents

--- local-test/post-order.sh
+++ local-test/post-order.sh
@@ -1,2 +1,3 @@
 cd ..
-sam local invoke "PostOrders" -e events/post-order-event.json -n envs/env.json
+# sam local invoke "PostOrders" -e events/post-order-event.json -n envs/env.json
+sam local invoke "PostOrders" -e events/post-order-sqs-event.json -n envs/env.json
